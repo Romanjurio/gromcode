da5a98d94289fa063a411970abba0c312fb6fd61
Roman Pylypchak <74046891+Romanjurio@users.noreply.github.com>
2022-11-22 13:04:33 +0200

    Add files via upload
    add dom

---
+++ DOM/task2/index.js
@@ -0,0 +1,11 @@
+export const getTitleElement = () => {
+    console.dir(document.querySelector('.title'));
+};
+
+getTitleElement();
+
+export const getInputElement = () => {
+  console.dir(document.querySelector('input', 'text'))
+}
+
+getInputElement()
